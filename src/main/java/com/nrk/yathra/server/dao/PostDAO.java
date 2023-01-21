5f959027023fce3cab22184ce5ab5cdd59f40cd9
Nelvin <nelvin4u17@gmail.com>
2023-01-21 19:53:16 +0530

    api changes added

--- src/main/java/com/nrk/yathra/server/dao/PostDAO.java
+++ src/main/java/com/nrk/yathra/server/dao/PostDAO.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Component
--- src/main/java/com/nrk/yathra/server/dto/User.java
+++ src/main/java/com/nrk/yathra/server/dto/User.java
@@ -20,5 +20,6 @@ public class User {
     private String userName;
     private String userEmail;
     private  String userPassword;
+    private  String userRole;
 
 }
